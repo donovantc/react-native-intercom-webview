16d04675e680215685a78f32c4339162b579f4a9
Donovan Isherwood <donovanisherwood@MAC70.local>
2017-01-18 13:21:47 +0100

    Added loader and prop types

--- IntercomWebView.js
+++ IntercomWebView.js
@@ -1,8 +1,16 @@
-import React, {Component} from 'react';
-import { View, WebView, Text } from 'react-native';
+import React, {Component } from 'react';
+import { View, WebView } from 'react-native';
+import Spinner from 'react-native-loading-spinner-overlay';
 
 
-export default class IntercomWebView extends Component{
+class IntercomWebView extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            isLoading: true
+        };
+    }
+
     injectedJS = (appId, name, email, hideLauncher) => {
         return `
             window.Intercom('boot', {
@@ -16,26 +24,54 @@ export default class IntercomWebView extends Component{
                 window.Intercom('showMessages');`;
     }
 
+    onLoadEnd = () => {
+        this.setState({isLoading: false});
+
+        if (this.props.onLoadEnd)
+            this.props.onLoadEnd();
+    }
+
     render(){
-        const { appId, name, email, hideLauncher, defaultHeight } = this.props;
-        
+        const { appId, name, email, hideLauncher, defaultHeight, showLoadingOverlay, loaderColor, ...remainingProps } = this.props;
+        const { isLoading } = this.state;
+
+        console.log(isLoading);
+
         return(
 
             <View style={[style.containerStyle, {height: defaultHeight}, this.props.style]}>
+                <Spinner visible={showLoadingOverlay && isLoading} />
                 <WebView source={require('./IntercomWebView.html')}
                          injectedJavaScript={this.injectedJS( appId, name, email, hideLauncher )}
                          javaScriptEnabled={true}
-                         domStorageEnabled={true}/>
+                         domStorageEnabled={true}
+                         onLoadEnd={this.onLoadEnd}
+                        {...remainingProps}
+                />
             </View>
         )
     }
 }
-    
+
 const style = {
     containerStyle: {
-        height: 500,
-        flex: 1
-    }    
-}    
+        height: 500
+    }
+}
 
+IntercomWebView.PropTypes = {
+    appId: React.PropTypes.string,
+    name: React.PropTypes.string,
+    email: React.PropTypes.string,
+    hideLauncher: React.PropTypes.bool,
+    showLoadingOverlay: React.PropTypes.bool,
+    defaultHeight: React.PropTypes.number,
+    loaderColor: React.PropTypes.string
+};
 
+IntercomWebView.defaultProps = {
+    hideLauncher: false,
+    showLoadingOverlay: true
+};
+
+export default IntercomWebView;
